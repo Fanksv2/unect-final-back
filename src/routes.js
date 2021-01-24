74e523ac27df0cec124d9e313adcb6b37413d620
YukioVN <noma@alunos.utfpr.edu.br>
2021-01-24 18:48:35 -0300

    Added get route for fetching todo-tasks

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const TaskController = require('./controller/TaskController');
 const router = express.Router();
 
 router.post("/create", TaskController.create);
+router.get("/task", TaskController.fetchTask);
 
 module.exports = router;
 
