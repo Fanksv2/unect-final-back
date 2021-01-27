73a80216d45ffafed3c108fc9ac44f0a2bfb00d8
Fanksv2 <fanksv2@gmail.com>
2021-01-26 21:30:23 -0300

    Added - Pass todo task to done task

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,3 +1,4 @@
+const { text } = require('body-parser');
 const Task = require('../models/Task');
 
 module.exports = {
@@ -21,5 +22,16 @@ module.exports = {
     async fetchDoneTask(req, res) {
         const task = await Task.find({'content.status':'done'}).sort('-createdAt');
         return res.json(task);
+    },
+    async checkTask(req, res){
+        const {data} = req.body;
+        const {id} = data;
+        const updated = await Task.findByIdAndUpdate(id, {
+            "content.status" : "done"
+        },
+        {
+            new : true
+        })
+        return res.json(updated);
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/create", TaskController.create);
 router.get("/todo", TaskController.fetchTodoTask);
 router.get("/done", TaskController.fetchDoneTask);
+router.post("/check", TaskController.checkTask)
 
 module.exports = router;
