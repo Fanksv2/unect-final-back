9dcd580c20162247fe0d59b81ceda5a567a4f671
YukioVN <noma@alunos.utfpr.edu.br>
2021-01-28 18:30:40 -0300

    Added Patch route to update Tasks

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,5 @@ router.get("/todo", TaskController.fetchTodoTask);
 router.get("/done", TaskController.fetchDoneTask);
 router.post("/check", TaskController.checkTask)
 router.delete("/delete",TaskController.deleteTask);
+router.patch("/update", TaskController.updateTask);
 module.exports = router;
